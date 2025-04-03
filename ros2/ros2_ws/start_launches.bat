2a6339e2b8e82fbb1300852a58f7bee9187bbdcb
김동욱 <ehddnr0929@naver.com>
2025-04-03 17:26:22 +0900

    feat: 배치 파일 수정 (빌드추가)

--- ros2/ros2_ws/start_launches.bat
+++ ros2/ros2_ws/start_launches.bat
@@ -1,7 +1,9 @@
 @echo off
 rem ROS2 환경 설정
 call C:\dev\ros2-foxy\setup.bat
-call C:\Users\SSAFY\Desktop\S12P21D106\ros2\ros2_ws\install\local_setup.bat
+
+@REM call C:\Users\SSAFY\Desktop\S12P21D106\ros2\ros2_ws\install\local_setup.bat
+call .\install\local_setup.bat
 
 rem 0. integrated_launch.py 실행(통합 런치치 실행)
 rem start ros2 launch auto_driving_cpp integrated_launch.py
--- ros2/ros2_ws/start_nodes.bat
+++ ros2/ros2_ws/start_nodes.bat
@@ -1,7 +1,9 @@
 @echo off
 rem ROS2 환경 설정
 call C:\dev\ros2-foxy\setup.bat
-call C:\Users\SSAFY\Desktop\S12P21D106\ros2\ros2_ws\install\local_setup.bat
+
+@REM call C:\Users\SSAFY\Desktop\S12P21D106\ros2\ros2_ws\install\local_setup.bat
+call .\install\local_setup.bat
 
 rem 1. perception_cpp 패키지 노드들
 start ros2 run perception_cpp odom_unity
