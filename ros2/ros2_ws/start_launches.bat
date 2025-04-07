6315f328f93caf7f7a01cbb822428b5c9946bd2b
김동욱 <ehddnr0929@naver.com>
2025-04-07 10:12:42 +0900

    feat: 통합테스트 and 목표점 좌표변환

--- ros2/ros2_ws/start_launches.bat
+++ ros2/ros2_ws/start_launches.bat
@@ -16,12 +16,24 @@ start ros2 launch perception_cpp perception_launch.py
 rem 2. unity_dijkstra_launch.py 실행(판단/제어 실행)
 start ros2 launch auto_driving_cpp unity_dijkstra_launch.py
 
+
+start ros2 run topic_bridge_py mqtt_ros2_bridge
+
+start ros2 run topic_bridge_py ros2_mqtt_bridge
+
 rem 3. endpoint.py 실행(unity와 연결 실행)
 start ros2 launch ros_tcp_endpoint endpoint.py
 
 
+@REM start ros2 run perception_py opencv_yolo
+
+@REM start ros2 run perception_py fire_image_subscriber
+
+start ros2 run fire_control fire_suppression_node
+
+
 
 rem 4. goal_publisher 노드(좌표 입력을 쉽게 하기 위해 따로 실행)
-start ros2 run auto_driving_cpp goal_publisher  
+@REM start ros2 run auto_driving_cpp goal_publisher  
 
 pause
